a94fbc716041250282a45bb1c73ce47e01500a5d
irhspur <bajracharya.roop@gmail.com>
2020-09-10 18:48:29 +0545

    Feature: Initial data fetch implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+type TClub = {
+    code: string;
+    country: string;
+    name: string;
+};
+
+type TData = {
+    name: string;
+    clubs: TClub[];
+};
 
 const App = () => {
-    return <div>Hello World</div>;
+    const [data, setData] = useState<TData>();
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json"
+            );
+            const dataResponse = await response.json();
+            setData(dataResponse);
+        })();
+    }, []);
+
+    return (
+        <div>
+            <h1>{data?.name}</h1>
+            {data?.clubs.map((club) => (
+                <li key={club.code}>
+                    {club.name} - {club.country}
+                </li>
+            ))}
+        </div>
+    );
 };
 
 export default App;
