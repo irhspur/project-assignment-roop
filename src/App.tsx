cac00a6c8ac12172381b31b45e559fddae1dfa61
irhspur <bajracharya.roop@gmail.com>
2020-09-12 14:29:27 +0545

    Data formatted synced with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from "react";
 import { getClubs, getMatchDetails } from "./services/footballApi";
-import { TData, TMatchDetails } from "./utils";
+import { TData, TTeamData } from "./utils";
 import { getTeams } from "./utils/dataUtils";
+import { FixtureTable } from "./components/FixtureTable";
 
 const App = () => {
-    const [data, setData] = useState<TData>();
-    const [matches, setMatches] = useState<TMatchDetails>();
+    const [clubs, setClubs] = useState<TData>();
+    const [error, setError] = useState<string | null>(null);
+    const [matches, setMatches] = useState<TTeamData[]>([]);
 
     useEffect(() => {
         (async () => {
-            const dataResponse = await getClubs("2015-16");
-            setData(dataResponse);
-            const dataMatches = await getMatchDetails("2015-16");
-            setMatches(dataMatches);
+            try {
+                const clubsResponse = await getClubs("2015-16");
+                setClubs(clubsResponse);
+                const dataMatches = await getMatchDetails("2015-16");
+                setMatches(Object.values(dataMatches));
+            } catch (err) {
+                setError("Something Went Wrong");
+            }
         })();
     }, []);
 
-    if (!matches) return <div>Loading...</div>;
-    else {
-        return (
-            <div>
-                <h1>{matches?.name}</h1>
-                <h2>Teams</h2>
-                {getTeams(matches.rounds).map((team, idx) => (
+    // if (error) return <div>{error}</div>;
+    // else {
+    return (
+        <div>
+            <h1>{clubs?.name}</h1>
+            <FixtureTable data={matches} />
+            <h2>Clubs</h2>
+            {/* {getTeams(matches.rounds).map((team, idx) => (
                     <li key={idx}>{team}</li>
-                ))}
-                <h1>{data?.name}</h1>
-                {data?.clubs.map((club) => (
-                    <li key={club.code}>
-                        {club.name} - {club.country}
-                    </li>
-                ))}
-            </div>
-        );
-    }
+                ))} */}
+            {clubs?.clubs.map((club) => (
+                <li key={club.code}>
+                    {club.name} - {club.country}
+                </li>
+            ))}
+        </div>
+    );
+    // }
 };
 
 export default App;
