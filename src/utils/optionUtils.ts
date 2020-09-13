c6cdec96a7a332e665ad7d2ffd5b4d15920a662c
irhspur <bajracharya.roop@gmail.com>
2020-09-13 15:01:04 +0545

    Utility: Seasons added to option utils

--- src/utils/optionUtils.ts
+++ src/utils/optionUtils.ts
@@ -1,4 +1,6 @@
 export const seasons = [
+    '2013-14',
+    '2014-15',
     '2015-16',
     '2016-17',
     '2017-18',
@@ -8,3 +10,5 @@ export const seasons = [
     '2020',
     '2020-21'
 ]
+
+export const initialSeason = seasons[0]
